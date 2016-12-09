dadd6d7f10a0c0cdcf25dcb6c14d77c4ee8dc358
mehmetartun <mehmet@artun.com>
2016-12-09 16:37:24 +0000

    Changed db name

--- utils/embx_dbconn.php
+++ utils/embx_dbconn.php
@@ -3,10 +3,10 @@
 define("MYSQL_HOST","127.0.0.1");
 define("MYSQL_USER","root");
 define("MYSQL_PASS","emb");
-define("MYSQL_DB","embondsnew");
-define("AUDIO_FILE_PATH",'/Library/WebServer/Documents/embx-b/downloads/');
-define("EMB_SOURCE_FILE_DIRECTORY","/Library/WebServer/Documents/embx-b/source_files/");
-define("EMB_UPLOAD_DIRECTORY","/Library/WebServer/Documents/embx-b/uploads/");
+define("MYSQL_DB","embondsworking");
+define("AUDIO_FILE_PATH",'~mehmetartun/emb-analytics/downloads/');
+define("EMB_SOURCE_FILE_DIRECTORY","~mehmetartun/emb-analytics/source_files/");
+define("EMB_UPLOAD_DIRECTORY","~mehmetartun/emb-analytics/uploads/");
 define("TESTISIN","XSTEST123456");
 
 
@@ -19,7 +19,7 @@ $cwd = getcwd();
 
 
 
-ini_set('upload_tmp_dir','/Library/WebServer/Documents/embx-b/uploads/');
+ini_set('upload_tmp_dir','~mehmetartun/emb-analytics/uploads/');
 
 // Create connection
 //$conn = mysqli_connect($servername, $username, $password,$database);
